652febdbc1b238c43d9de62c28bb752df6b657a3
justsushant <sushantcod3s@gmail.com>
2023-09-17 10:06:58 +0530

    reading files from fs

---
+++ blogposts_test.go
@@ -0,0 +1,23 @@
+package blogposts_test
+
+import (
+	blogposts "github.com/justsushant/blogposts"
+	"testing"
+	"testing/fstest"
+)
+
+func TestNewBlogPosts(t *testing.T) {
+	fs := fstest.MapFS{
+		"hello world.md":  {Data: []byte("hi")},
+		"hello-world2.md": {Data: []byte("hola")},
+	}
+
+	posts, err := blogposts.NewPostsFromFS(fs)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if len(posts) != len(fs) {
+		t.Errorf("got %d posts, wanted %d posts", len(posts), len(fs))
+	}
+}
