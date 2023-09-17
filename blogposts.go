ab1b1284e2abecbab92a58f1cc854039746ce68d
justsushant <sushantcod3s@gmail.com>
2023-09-17 11:58:43 +0530

    extracting title from post file

--- blogposts.go
+++ blogposts.go
@@ -4,10 +4,6 @@ import (
 	"io/fs"
 )
 
-type Post struct {
-
-}
-
 func NewPostsFromFS(fileSystem fs.FS) ([]Post, error) {
 	dir, err := fs.ReadDir(fileSystem, ".")
 	if err != nil {
@@ -15,9 +11,25 @@ func NewPostsFromFS(fileSystem fs.FS) ([]Post, error) {
 	}
 
 	var posts []Post
-	for range dir {
-		posts = append(posts, Post{})
+	for _, f := range dir {
+		post, err := getPost(fileSystem, f.Name())
+		if err != nil {
+			return nil, err //todo: needs clarification, should we totally fail if one file fails? or just ignore?
+		}
+
+		posts = append(posts, post)
 	}
 
 	return posts, nil
 }
+
+func getPost(fileSystem fs.FS, fileName string) (Post, error) {
+	postFile, err := fileSystem.Open(fileName)
+	if err != nil {
+		return Post{}, err
+	}
+	defer postFile.Close()
+
+	return newPost(postFile)	
+}
+
